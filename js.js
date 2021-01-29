1f31f40e617c56f8232e8a19ad6566a8ddbab7e6
ElitTeam29 <78167565+ElitTeam29@users.noreply.github.com>
2021-01-29 23:57:57 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,3 @@
+let name = prompt("Dear Visitor,Please Enter Your Name ", "");
+let enter_place = document.getElementById("introduction");
+enter_place.innerHTML = '<span style = "font-size: 70px;font-family: cursive; color: rgb(255,216,1);">Dear, ' + name
